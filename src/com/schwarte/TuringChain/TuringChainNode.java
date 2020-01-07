f42fb467172668adc958dbe25b20d6d0491c3a45
Joachim Schwarte <joachim.schwarte@iwb.uni-stuttgart.de>
2020-01-07 10:50:32 +0100

    Version 0.2 (Modifier korrigiert; Print-Befehle auskommentiert)

--- src/com/schwarte/TuringChain/TuringChainNode.java
+++ src/com/schwarte/TuringChain/TuringChainNode.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class TuringChainNode {
 	
 	private static int innerStatus = 0;
-    static LinkedList<TuringChainNode> nodeList = new LinkedList<TuringChainNode>();
+    private static LinkedList<TuringChainNode> nodeList = new LinkedList<TuringChainNode>();
 	
 	private int cellContent;
 
@@ -22,36 +22,36 @@ public class TuringChainNode {
 	public void activateNode(int i, TuringTable tt) {
 		int ttLength = tt.getRowcount();
 		for (int j=0; j<ttLength; ++j) {
-			System.out.println("i= " + i
-					+ " innerStatus: " + innerStatus 
-					+ " cellContent: " + cellContent
-					);
+//			System.out.println("i= " + i
+//					+ " innerStatus: " + innerStatus 
+//					+ " cellContent: " + cellContent
+//					);
 			if ((tt.getRow(j).getPreInnerStatus() == innerStatus) &&
 					(tt.getRow(j).getPreCellContent() == cellContent)) {
 				cellContent = tt.getRow(j).getPostCellContent();
 				innerStatus = tt.getRow(j).getPostInnerStatus();
-				System.out.println("i= " + i
-						+ " innerStatus: " + innerStatus 
-						+ " cellContent: " + cellContent
-						);
-				System.out.println("i= " + i 
-						+ " IV: " + tt.getRow(j).getPreInnerStatus() 
-						+ " BV: " + tt.getRow(j).getPreCellContent()
-						+ " IN: " + tt.getRow(j).getPostInnerStatus()
-						+ " BN: " + tt.getRow(j).getPostCellContent()
-						);
+//				System.out.println("i= " + i
+//						+ " innerStatus: " + innerStatus 
+//						+ " cellContent: " + cellContent
+//						);
+//				System.out.println("i= " + i 
+//						+ " IV: " + tt.getRow(j).getPreInnerStatus() 
+//						+ " BV: " + tt.getRow(j).getPreCellContent()
+//						+ " IN: " + tt.getRow(j).getPostInnerStatus()
+//						+ " BN: " + tt.getRow(j).getPostCellContent()
+//						);
 				if (tt.getRow(j).getNextAction().equals(TuringAction.R)) {
-					System.out.println("Node: " + i + "   Action: R");
+//					System.out.println("Node: " + i + "   Action: R");
 					getNode(i+1).activateNode(i+1, tt);
 				}
 				if (tt.getRow(j).getNextAction().equals(TuringAction.L)) {
-					System.out.println("Node: " + i + "   Action: L");
+//					System.out.println("Node: " + i + "   Action: L");
 					getNode(i-1).activateNode(i-1, tt);	
 				}
 				if (tt.getRow(j).getNextAction().equals(TuringAction.Stop)) {
-					System.out.println("Node: " + i + "   Action: Stop");
+//					System.out.println("Node: " + i + "   Action: Stop");
 				}
-				System.out.println("Node: " + i + "   return");
+//				System.out.println("Node: " + i + "   return");
 				return;
 			}			
 		}		
