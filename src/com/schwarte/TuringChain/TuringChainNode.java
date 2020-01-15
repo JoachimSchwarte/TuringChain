5415511ee6c3549af612b77e1257df9983f6389f
Joachim Schwarte <joachim.schwarte@iwb.uni-stuttgart.de>
2020-01-15 11:23:33 +0100

    Version 0.3 (neue Klasse: TuringMachine)

--- src/com/schwarte/TuringChain/TuringChainNode.java
+++ src/com/schwarte/TuringChain/TuringChainNode.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 
 public class TuringChainNode {
 	
-	private static int innerStatus = 0;
     private static LinkedList<TuringChainNode> nodeList = new LinkedList<TuringChainNode>();
 	
 	private int cellContent;
@@ -19,42 +18,18 @@ public class TuringChainNode {
 		return nodeList.get(i);
 	}
 	
-	public void activateNode(int i, TuringTable tt) {
+	public TuringAction activateNode(int i, int innerStatus, TuringTable tt) {
 		int ttLength = tt.getRowcount();
 		for (int j=0; j<ttLength; ++j) {
-//			System.out.println("i= " + i
-//					+ " innerStatus: " + innerStatus 
-//					+ " cellContent: " + cellContent
-//					);
 			if ((tt.getRow(j).getPreInnerStatus() == innerStatus) &&
 					(tt.getRow(j).getPreCellContent() == cellContent)) {
 				cellContent = tt.getRow(j).getPostCellContent();
+				TuringMachine.getInstanceTuringMachine().setInnerStatus(tt.getRow(j).getPostInnerStatus());
 				innerStatus = tt.getRow(j).getPostInnerStatus();
-//				System.out.println("i= " + i
-//						+ " innerStatus: " + innerStatus 
-//						+ " cellContent: " + cellContent
-//						);
-//				System.out.println("i= " + i 
-//						+ " IV: " + tt.getRow(j).getPreInnerStatus() 
-//						+ " BV: " + tt.getRow(j).getPreCellContent()
-//						+ " IN: " + tt.getRow(j).getPostInnerStatus()
-//						+ " BN: " + tt.getRow(j).getPostCellContent()
-//						);
-				if (tt.getRow(j).getNextAction().equals(TuringAction.R)) {
-//					System.out.println("Node: " + i + "   Action: R");
-					getNode(i+1).activateNode(i+1, tt);
-				}
-				if (tt.getRow(j).getNextAction().equals(TuringAction.L)) {
-//					System.out.println("Node: " + i + "   Action: L");
-					getNode(i-1).activateNode(i-1, tt);	
-				}
-				if (tt.getRow(j).getNextAction().equals(TuringAction.Stop)) {
-//					System.out.println("Node: " + i + "   Action: Stop");
-				}
-//				System.out.println("Node: " + i + "   return");
-				return;
+				return tt.getRow(j).getNextAction();
 			}			
-		}		
+		}
+		return null;			
 	}
 
 	public int getCellContent() {
---
+++ src/com/schwarte/TuringChain/TuringMachine.java
@@ -0,0 +1,32 @@
+package com.schwarte.TuringChain;
+
+public class TuringMachine {
+
+	private int position = 0;
+	private int innerStatus = 0;
+	private static TuringMachine instance = null;
+	
+	private TuringMachine() {
+		super();
+	}
+	
+	public static TuringMachine getInstanceTuringMachine() {
+		if (instance == null) {
+			instance = new TuringMachine();
+		}
+		return instance; 
+	}
+	
+	public void startMachine(TuringTable tt) {
+		TuringAction ta = TuringAction.R;
+		while (ta.equals(TuringAction.Stop)==false) {
+			ta = TuringChainNode.getNode(position).activateNode(position, innerStatus, tt);
+			if (ta.equals(TuringAction.R) == true) position++; 
+			if (ta.equals(TuringAction.L) == true) position--; 
+		}
+	}
+
+	public void setInnerStatus(int innerStatus) {
+		this.innerStatus = innerStatus;
+	}
+}
